8ceadda23d4bc424403c528d696c3e48e95a613e
Ron <rmarrero22@mail.valenciacollege.edu>
2016-04-07 22:44:45 -0400

    Secondary commit.

---
+++ Problem 2 - Divisors/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2___Divisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter your X: ");
+                int x = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Enter your Y: ");
+                int y = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Enter your N: ");
+                int n = Int32.Parse(Console.ReadLine());
+
+                ArrayList numbersWithNDivisors = FindNDivisors(x, y, n);
+
+                foreach (int number in numbersWithNDivisors)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+        }
+
+        private static ArrayList FindNDivisors(int x, int y, int n)
+        {
+            ArrayList numbers = new ArrayList();
+
+            for (int i = x; i <= y; i++)
+            {
+                int count = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                        if (i * i == j)
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count == n)
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ Problem 3 - Emirp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3___Emirp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter your X: ");
+                int x = Int32.Parse(Console.ReadLine().ToString());
+
+                Console.Write("Enter your Y: ");
+                int y = Int32.Parse(Console.ReadLine().ToString());
+
+                for (int i = x; i <= y; i++)
+                {
+                    if (IsPrimeNumber(i) && IsPalindromePrime(i))
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        static bool IsPrimeNumber(int number)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count > 2)
+                return false;
+
+            else
+                return true;
+        }
+
+        static bool IsPalindromePrime(int number)
+        {   
+            string numberString = ((Int32)number).ToString();
+            int numberLength = numberString.Length;
+
+            ArrayList palindromeArray = new ArrayList();
+
+            for (int i = 0; i < numberLength; i++)
+            {
+                palindromeArray.Add(numberString.Substring(numberString.Length - 1));
+                numberString = numberString.Substring(0, numberString.Length - 1);
+            }
+
+            string t = "";
+
+            foreach (string n in palindromeArray)
+            {
+                t += n;
+            }
+
+            int palindrome = Int32.Parse(t);
+            return IsPrimeNumber(palindrome);
+        }
+    }
+}
